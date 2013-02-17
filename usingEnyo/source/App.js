a5089333bd3b618537fe60a94e39bf25a1fa8ae3
Alex Marenin <great_muchacho@mail.ru>
2013-02-17 20:46:11 +0400

    Force tab panels to work. Need to move it into a separate kind.

--- usingEnyo/source/App.js
+++ usingEnyo/source/App.js
@@ -9,24 +9,39 @@ enyo.kind({
     fit: true,
     components: [
         {kind: 'NavToolbar', onBack: 'goBack', showBack: true, caption: 'User Settings' },
-        {kind: 'onyx.RadioGroup', layoutKind: 'enyo.ColumnsLayout', onActivate: 'switchTabs', controlClasses: 'onyx-tabbutton', components: [
-            {content: 'Call Flow', name: 'callFlowButton', active: true },
-            {content: 'Caller ID', name: 'callerIdButton'},
-            {content: 'Fax', name: 'faxButton'}
+        {kind: 'onyx.RadioGroup', layoutKind: 'enyo.ColumnsLayout', onActivate: 'onTabActivate', controlClasses: 'onyx-tabbutton', components: [
+            {name: 'callFlowButton', content: 'Call Flow', active: true, bindTo: 'callFlowPanel' },
+            {name: 'callerIdButton', content: 'Caller ID', bindTo: 'callerIdPanel' },
+            {name: 'faxButton', content: 'Fax', bindTo: 'faxPanel' }
         ]},
-        {kind: 'enyo.Scroller', fit: true, components: [
-            {name: 'main', classes: 'nice-padding', allowHtml: true},
-            {kind: 'UserInfo'}
+        {kind: 'Panels', fit: true, realtimeFit: true, onTransitionFinish: 'onPanelActivate', controlClasses: 'nice-padding', components: [
+            {name: 'callFlowPanel', content: 'callFlowPanel', active: true, bindTo: 'callFlowButton' },
+            {name: 'callerIdPanel', content: 'callerIdPanel', bindTo: 'callerIdButton' },
+            {name: 'faxPanel', content: 'faxPanel', bindTo: 'faxButton' }
         ]}
     ],
 
-    goBack: function( inSender, inEvent ){
+    goBack: function(){
         this.log( 'Okay... I going back' );
         window.history.back();
     },
 
-    switchTabs: function( inSender, inEvent ){
-        if ( inEvent.originator.getActive() )
-            this.log( inEvent.originator.getContent() );
+    onTabActivate: function( inSender, inEvent ){
+        if ( inEvent.originator.getActive() ){
+            var panelName = inEvent.originator.bindTo,
+                panel = this.$[panelName],
+                parent = panel.parent,
+                isActive = panel.parent.getActive() === panel;
+
+            if ( !isActive )
+                parent.setIndex( parent.children.indexOf(panel) );
+        }
+    },
+
+    onPanelActivate: function( inSender, inEvent ){
+        var buttonName = this.$.panels.getActive().bindTo,
+            button = this.$[buttonName];
+        if ( !button.getActive() )
+            button.setActive();
     }
 });
