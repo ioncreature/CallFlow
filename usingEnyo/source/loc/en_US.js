1573320f92275cd4897777152bbc38881db42739
alexander.marenin <great_muchacho@mail.ru>
2013-02-21 23:22:14 +0400

    Added localization

---
+++ usingEnyo/source/loc/en_US.js
@@ -0,0 +1,21 @@
+/**
+ * @author Marenin Alexander
+ * February 2013
+ */
+
+
+var loc = {
+    CallFlow: {
+        blockUnwantedCallers: 'Block Unwanted Callers',
+        answeringRules: 'Answering Rules',
+        answeringRulesDesc: 'System behavior based on time of the day and day of the week.',
+        greetTheCaller: 'Greet the Caller',
+        greetTheCallerDesc: 'First thing that callers will hear after system picks up incoming call.',
+        screenTheCaller: 'Screen the Caller',
+        screenTheCallerDesc: 'Ask callers to say their name before connecting.',
+        connecting: 'Connecting',
+        connectingDesc: 'Callers will hear a message that they are being connected to your extension.',
+        playing: 'Playing',
+        playingDesc: 'What users will hear while call comes through.'
+    }
+};
